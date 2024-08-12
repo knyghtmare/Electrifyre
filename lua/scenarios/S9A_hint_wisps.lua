894ea3c8c8a019dec2b7e9be4ae8b51c5daff90c
Tahsin Jahin Khalid <5283677+knyghtmare@users.noreply.github.com>
2024-08-12 18:17:21 +0600

    Create S9A_hint_wisps.lua

---
+++ lua/scenarios/S9A_hint_wisps.lua
@@ -0,0 +1,37 @@
+local device_coords = {
+    {25, 29}, {21, 36}, {10, 39},
+    {5, 22}, {42, 21}, {41, 39}
+}
+
+for i = 1, #device_coords do
+    local device = device_coords[i]
+    local x_val = device[1]
+    local y_val = device[2]
+
+    -- scroll to the Seer
+    wesnoth.interface.scroll_to_hex(
+        28, 53, false, true)
+
+    -- animate her
+    seer_unit = wesnoth.units.find_on_map{ id = 'Vanleneada' }
+    animation = wesnoth.units.create_animator() 
+
+    animation:add(seer_unit[1], "attack", "miss")
+    animation:run()
+
+    animation:clear()
+
+    wesnoth.interface.delay(400)
+
+    -- scroll to hex
+    wesnoth.interface.scroll_to_hex(
+        device[1], device[2], false, true)
+
+    -- play magic
+    wesnoth.audio.play("magic-faeriefire-miss.ogg")
+
+    wesnoth.sides.remove_shroud(1, {{device[1],device[2]}})
+    wesnoth.sides.remove_shroud(2, {{device[1],device[2]}})
+
+    wesnoth.interface.delay(400)
+end
